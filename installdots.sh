8014b4e760fff0b5fd7aeace9cba6a9175c1385e
Michael Snead <msnead@ws-msnead8.payformance.net>
2014-12-17 15:50:12 -0500

    Create bundle folder. Fix installdots

--- installdots.sh
+++ installdots.sh
@@ -5,8 +5,6 @@
 # Adapted from http://blog.smalleycreative.com/tutorials/using-git-and-github-to-manage-your-dotfiles/
 ############################
 
-mkdir ../sauce_old
-
 ########## Variables
 
 dir=~/sauce                    # dotfiles directory
@@ -15,9 +13,10 @@ files="bashrc vimrc vim tmux.conf bash_git minttyrc gitconfig"    # list of file
 
 ##########
 
-# create dotfiles_old in homedir
+# create sauce_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
+mkdir -p $dir/.vim/bundle
 echo "...done"
 
 # change to the dotfiles directory
@@ -25,10 +24,10 @@ echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to sauce_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file ~/sauce_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/.$file ~/.$file
 done
