ae8ca393c53cdaceecc6fc854a326a0ac5af27d6
Michael <aikeru@gmail.com>
2014-12-20 15:16:48 -0500

    New setup script

---
+++ setup.ps1
@@ -0,0 +1,35 @@
+# setup.ps1
+#   This is intended to...
+#   1. Setup cygwin with all packages
+#   2. Setup ubuntu font
+#   3. Anything else that needs setting up on Windows profile
+
+$osArch = (gwmi win32_operatingsystem | select osarchitecture).osarchitecture
+
+if($osArch -eq "32-bit") {
+    $download = "http://cygwin.com/setup-x86.exe";
+    $cygInstallFolder = "c:\cygwin";
+} else {
+    $download = "http://cygwin.com/setup.exe";
+    $cygInstallFolder = "c:\cygwin64";
+}
+$TempCygDir = "$env:temp\cygInstall";
+$mirror = "http://mirror.nyi.net/cygwin"; 
+
+# Install Cygwin
+if(!(Test-Path -Path $TempCygDir -PathType Container)) {
+    $null = New-Item -Type Directory -Path $TempCygDir -Force
+}
+
+$packagesStr = ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aikeru/sauce/master/packages.txt'))
+$packages = ($packagesStr -split '[\r\n]') |? {$_}
+$packageList = [String]::Join(" ", $packages);
+
+$client = new-object System.Net.WebClient
+$client.DownloadFile($download, "$TempCygDir\setup.exe");
+Start-Process -wait -FilePath "$TempCygDir\setup.exe" -ArgumentList ("-q -n -l $TempCygDir -s " + $mirror + " -R " + $cygInstallFolder + " -P " + $packageList);
+
+"Cygwin should now be installed."
+
+
+
