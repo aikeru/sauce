1f63344ad3eef92ef009fcb1fdfa2fb16ae19805
aikeru <Michael.Snead@hotmail.com>
2014-12-27 13:17:13 -0500

    Organize files

--- setup.ps1
+++ setup.ps1
@@ -34,5 +34,5 @@ Start-Process -wait -FilePath "$TempCygDir\setup.exe" -ArgumentList ("-q -n -l $
 
 "Cygwin should now be installed."
 
-.\Add-Font.ps1 -Path ".\Ubuntu Mono for Powerline_0.ttf"
+./scripts/Add-Font.ps1 -Path ".\assets\Ubuntu Mono for Powerline_0.ttf"
 
